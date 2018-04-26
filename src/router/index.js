0cf54962e24fe1cae0da21156577b32d1fc63751
心叶 <yelloxing@gmail.com>
2018-04-26 11:13:19 +0800

    添加文章中备注

--- src/router/index.js
+++ src/router/index.js
@@ -3,21 +3,21 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 //路由跳转的组件，要提前注入
-import PageOne from '../components/PageOne.vue';
+import PageOne from '../components/PageOne.vue';//【地方一】
 import PageTwo from '../components/PageTwo.vue';
 
 //路由配置
 const router = new VueRouter({
     routes: [{
-        path: '/',//dsfs
+        path: '/',//【地方二】
         redirect: 'PageOneLink'
     },
     {
-        path: '/PageOneLink',
+        path: '/PageOneLink',//【地方三】
         component: PageOne
     },
     {
-        path: '/PageTwoLink',
+        path: '/PageTwoLink',//【地方四】
         component: PageTwo
     }
     ]
