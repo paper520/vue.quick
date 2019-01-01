cb8806696469d957fae4dabff97bbded6b85a75a
心叶 <yelloxing@gmail.com>
2019-01-01 16:49:27 +0800

    Vue2学习之旅六：添加对scss的支持

--- src/components/PageTwo.vue
+++ src/components/PageTwo.vue
@@ -1,11 +1,11 @@
 <template>
-    <section>
-        <input type="text" v-model="justDoIt">
-        <div>
-            输入的数据：{{justDoIt}}
-        </div>
-        <input type="button" value="DoIt" v-on:click="doIt()">
-    </section>
+  <section>
+    <input type="text" v-model="justDoIt">
+    <div>
+      输入的数据：{{justDoIt}}
+    </div>
+    <input type="button" value="DoIt" v-on:click="doIt()">
+  </section>
 </template>
 <script>
 export default {
@@ -20,13 +20,18 @@ export default {
       alert(this.justDoIt);
     }
   },
-  watch:{
-      justDoIt:function(newval, oldval){
-          console.log("justDoIt改变了，新值为：" + newval + ",旧值为：" + oldval);
-      }
+  watch: {
+    justDoIt: function(newval, oldval) {
+      console.log("justDoIt改变了，新值为：" + newval + ",旧值为：" + oldval);
+    }
   }
 };
 </script>
-<style>
 
+<style lang="scss" scoped>
+section {
+  & > div {
+    font-size: 20px;
+  }
+}
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,34 @@ module.exports = {
         path: __dirname,
         filename: 'build/main.js'
     },
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.js'
+        }
+    },
     module: {
         loaders: [{
                 test: /\.vue$/,
-                loader: 'vue-loader'
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        scss: 'style-loader!css-loader!sass-loader'
+                    }
+                }
             },
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/
+            },{
+                test: /\.css$/,
+                loader: ['style-loader', 'css-loader']
+            }, {
+                test: /\.scss$/,
+                loader: ['style-loader', 'css-loader', 'sass-loader']
+            }, {
+                test: /\.(png|jpg|jpeg|gif|bmp)$/,
+                loader: ['file-loader?limit=7000&name=build/assets/[name].[ext]']
             }
         ]
     }
